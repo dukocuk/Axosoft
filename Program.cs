7f22f03b072e09359b00254c20662dc562595cff
s147153 <s147153@student.dtu.dk>
2020-08-26 15:33:30 +0200

    created project base

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Axosoft
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Axosoft bruger oprettelse\r");
+            Console.WriteLine("------------------------\n");
+            //Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Request.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using dotenv.net.Utilities;
+namespace Axosoft
+{
+    public class Request
+    {
+        
+        private string clientID, clientSecret;
+        
+
+        public Request()
+        {
+            var envReader = new EnvReader();
+            this.clientID = envReader.GetStringValue("clientID");
+            this.clientSecret = envReader.GetStringValue("clientSecret");
+        }
+
+        public Request(string clientID, string clientSecret)
+        {
+            this.clientID = clientID;
+            this.clientSecret = clientSecret;
+        }
+    }
+}
